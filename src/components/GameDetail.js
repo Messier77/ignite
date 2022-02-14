4b3e346731d3d2780aad6bb47843e8c91c6e19f0
Messier77 <Messier77@users.noreply.github.com>
2022-02-14 18:28:30 +0200

    17. component transition

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -6,7 +6,7 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { smallImage } from "../util";
 
-const GameDetail = () => {
+const GameDetail = ({ pathId }) => {
   const navigate = useNavigate();
   // exit detail
   const exitDetailHandler = e => {
@@ -22,10 +22,10 @@ const GameDetail = () => {
     <>
       {!isLoading && (
         <CardShadow className="shadow" onClick={exitDetailHandler}>
-          <Detail>
+          <Detail layoutId={pathId}>
             <Stats>
               <div className="rating">
-                <h3>{game.name}</h3>
+                <motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
                 <p>Rating: {game.rating}</p>
               </div>
               <Info>
@@ -38,7 +38,8 @@ const GameDetail = () => {
               </Info>
             </Stats>
             <Media>
-              <img
+              <motion.img
+                layoutId={`image ${pathId}`}
                 src={smallImage(game.background_image, 1280)}
                 alt={game.background_image}
               />
@@ -70,6 +71,7 @@ const CardShadow = styled(motion.div)`
   position: fixed;
   top: 0;
   left: 0;
+  z-index: 2;
   &::-webkit-scrollbar {
     width: 0.5rem;
   }
